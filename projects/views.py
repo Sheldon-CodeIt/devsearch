aed0af231bdcd7eba9239c7f03ab43445bd8b86e
Sheldon <sheldonchettiar3@gmail.com>
2024-02-18 14:57:17 +0530

    rendering data in template

--- projects/views.py
+++ projects/views.py
@@ -3,9 +3,43 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+projectsLst = [
+    {
+        'id': '1',
+        'title': 'PhishFind',
+        'description': 'AI enabled phishing URLs detection using Machine Learning',
+        'topRated': True
+
+    }, 
+    {
+        'id': '2',
+        'title': 'Ecommerce Website',
+        'description': 'Fully funcitonal ecommerce website',
+        'topRated': False
+
+    }, 
+    {
+        'id': '3',
+        'title': 'Portfolio Website',
+        'description': 'A personal website to write articles and display achievements/ projects',
+        'topRated': False
+
+    }, 
+]
+
 def projects(request):
-    return render(request, 'projects/projects.html')
+    context = {'projects': projectsLst}
+    return render(request, 'projects/projects.html', context)
+
+
 
 def project(request, pk):
-    return render(request, 'projects/single-project.html')
+
+    projectObject = None
+
+    for i in projectsLst:
+        if i['id'] == str(pk):
+            projectObject = i
+
+    return render(request, 'projects/single-project.html', {"project": projectObject})
 
